76a090bb8f6bc8283b78288dd50e566750a5d3ad
Ben Mendis <bmendis@novetta.com>
2017-12-06 23:40:32 -0500

    day six

---
+++ 5/a_realloc.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import sys
+
+with open(sys.argv[1], 'r') as input_file:
+    line = input_file.readline()
+
+initial_state = [int(block) for block in line.split()]
+num_banks = len(initial_state)
+states = [' '.join([str(block) for block in initial_state])]
+
+count = 0
+print (count, states[0])
+current_state = initial_state
+while True:
+    count += 1
+
+    old_state = current_state
+    largest_bank_pos = 0
+    largest_bank_val = 0
+    for i in range(num_banks):
+        if old_state[i] > largest_bank_val:
+            largest_bank_pos = i
+            largest_bank_val = old_state[i]
+    current_state[largest_bank_pos] = 0
+    div_banks = largest_bank_val // num_banks
+    mod_banks = largest_bank_val % num_banks
+    for i in range(num_banks):
+        current_state[i] += div_banks
+    for i in range(mod_banks):
+        address = (largest_bank_pos + i + 1) % num_banks
+        current_state[address] += 1
+
+    bank_string = ' '.join([str(block) for block in current_state])
+    print (count, bank_string)
+    if bank_string in states:
+        break
+    else:
+        states.append(bank_string)
+
+print (count)
+
