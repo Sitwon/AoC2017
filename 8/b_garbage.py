d6a507141b1f5a702352f955f46745b5d8989a57
Ben Mendis <bmendis@novetta.com>
2017-12-09 00:45:30 -0500

    day nine

---
+++ 8/b_garbage.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import sys
+
+
+level = 0
+groups = []
+garbage_count = 0
+
+in_garbage = False
+in_bang = False
+
+with open(sys.argv[1], 'r') as input_file:
+    char = input_file.read(1)
+    while char:
+        if in_bang:
+            in_bang = False
+        elif in_garbage:
+            if char == '>':
+                in_garbage = False
+            elif char == '!':
+                in_bang = True
+            else:
+                garbage_count += 1
+        else:
+            if char == '<':
+                in_garbage = True
+            elif char == '{':
+                level += 1
+                groups.append(level)
+            elif char == '}':
+                level -= 1
+        char = input_file.read(1)
+
+print (garbage_count)
