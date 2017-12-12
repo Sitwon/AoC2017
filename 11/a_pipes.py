546dfa69f9ee9a90755964c9b4c8b66170e40726
Ben Mendis <bmendis@novetta.com>
2017-12-12 00:23:46 -0500

    day twelve

---
+++ 11/a_pipes.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import sys
+
+
+pid_pipes = {}
+with open(sys.argv[1], 'r') as input_file:
+    for line in input_file:
+        pid, pipes = map(lambda s: s.strip(), line.split('<->'))
+        pid = int(pid)
+        pipes = list(map(int, map(lambda s: s.strip(), pipes.split(', '))))
+        pid_pipes[pid] = pipes
+
+def add_to_group(pid, visited, in_group):
+    if pid in visited:
+        return visited, in_group
+    in_group += pid_pipes[pid]
+    visited.append(pid)
+    for ppid in pid_pipes[pid]:
+        visited, in_group = add_to_group(ppid, visited, in_group)
+    return visited, in_group
+
+visited, in_group = add_to_group(0, [], [])
+
+
+print(len(visted))
